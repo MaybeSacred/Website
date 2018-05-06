e2838579660b4bac6d8ccb66ddbace34a97a3693
Jonathan Tyson <jtyson79473@gmail.com>
2018-05-05 20:16:28 -0400

    switched qr code generation to a library that is .net core compatible

--- core/Server/site/QRGenerator.fs
+++ core/Server/site/QRGenerator.fs
@@ -1,25 +1,40 @@
 ﻿module QRGenerator 
 open Suave.FunctionalViewEngine
 open CommonHtml
-open QRCoder
 open Suave.Utils.Crypto
 open System.Text
 open FSharpx
 open Types
 open System
+open ZXing
+open ZXing.QrCode
+open System.Drawing.Imaging
+open System.Drawing
+open System.IO
 
-let encode size str = 
-    use gen = new QRCodeGenerator()
-    use dat = gen.CreateQrCode(str, QRCodeGenerator.ECCLevel.Q, forceUtf8=true)
-    use code = new Base64QRCode(dat)
-    code.GetGraphic(size)
-        
+let encode' size str =
+    let writer = 
+        new BarcodeWriterPixelData(
+            Format = BarcodeFormat.QR_CODE,
+            Options = new QrCodeEncodingOptions(Height=size, Width=size, Margin=0)
+    )
+    let data = writer.Write(contents=str)
+    use bitmap = new Bitmap(data.Width, data.Height, PixelFormat.Format32bppRgb)
+    use ms = new MemoryStream()
+    let bmData = bitmap.LockBits(new Rectangle(0,0,data.Width,data.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppRgb)
+    try 
+        System.Runtime.InteropServices.Marshal.Copy(data.Pixels, 0, bmData.Scan0, data.Pixels.Length)
+    finally
+        bitmap.UnlockBits(bmData)
+    bitmap.Save(ms, ImageFormat.Png)
+    ms.ToArray() |> Convert.ToBase64String
+
 let rand () = 
     let b = Array.zeroCreate<byte> 50
     cryptRandom.GetBytes b
     ASCIIEncoding.ASCII.GetString b
 
-let base64Qr () = "data:image/gif;base64," + (rand () |> encode 3)
+let base64Qr () = "data:image/gif;base64," + (rand () |> encode' 120)
 
 let page = {
     Title = "QR Code Generator" 
