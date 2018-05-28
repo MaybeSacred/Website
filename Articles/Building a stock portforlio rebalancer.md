5fb7379757d8b378b3f45a191103fd075e51b727
Jonathan Tyson <jtyson79473@gmail.com>
2018-05-28 18:05:54 -0400

    created sample grid

---
+++ Articles/Building a stock portforlio rebalancer.md
@@ -0,0 +1,1 @@
+[Initial TypeScript guide](https://blog.logrocket.com/how-why-a-guide-to-using-typescript-with-react-fffb76c61614)
--- core/typescript/app.tsx
+++ core/typescript/app.tsx
@@ -1,12 +1,45 @@
 import * as React from "react";
+import * as _ from "lodash";
 import * as ReactDOM from "react-dom";
-import FirstComponent from './FirstComponent';
-import UserComponent from './UserComponent';
+import * as ReactDataGrid from "react-data-grid";
+class Example extends React.Component {
+    _columns: any[];
+    _rows: any[];
+    constructor(props, context){
+        super(props,context);
+        this.createRows();
+        this._columns = [
+            { key: 'id', name: 'ID'},
+            { key: 'title', name: 'Title'},
+            { key: 'count', name: 'Count'}
+        ];
+        this.state = null;
+        this.rowGetter = this.rowGetter.bind(this);
+    }
+    createRows () {
+        this._rows = _.times(5, x => ({
+            id: x, 
+            title: 'Title ' + x, 
+            count: x *100}));
+    }
+    rowGetter (i: number) {
+        return this._rows[i];
+    }
+    render() {
+        return (
+            <ReactDataGrid
+                columns={this._columns}
+                rowGetter={this.rowGetter}
+                rowsCount={this._rows.length}
+                minHeight={500}
+                />
+        );
+    }
+}
 ReactDOM.render(
     <div>
         <h1>Hello, Welcome to the first page</h1>
-        <FirstComponent/>
-        <UserComponent name="Loggg" age={105} address="hahaha" dob={new Date()} />
+        <Example/>
     </div>,
     document.getElementById("root")
 );
