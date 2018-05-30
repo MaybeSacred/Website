63227317f083590a7c49d4c5c3995d6b9ba6b7f1
Jonathan Tyson <jtyson79473@gmail.com>
2018-05-29 22:46:06 -0400

    added immutability helpers and added example for editable rows

--- core/typescript/app.tsx
+++ core/typescript/app.tsx
@@ -2,36 +2,43 @@ import * as React from "react";
 import * as _ from "lodash";
 import * as ReactDOM from "react-dom";
 import * as ReactDataGrid from "react-data-grid";
+import * as immutability from "immutability-helper";
 class Example extends React.Component {
     _columns: any[];
-    _rows: any[];
+    state: {_rows: any[]};
     constructor(props, context){
         super(props,context);
         this.createRows();
         this._columns = [
-            { key: 'id', name: 'ID'},
-            { key: 'title', name: 'Title'},
-            { key: 'count', name: 'Count'}
+            { key: 'id', name: 'ID', editable: true },
+            { key: 'title', name: 'Title', editable: true },
+            { key: 'count', name: 'Count', editable: true }
         ];
-        this.state = null;
+        this.state = {_rows: this.createRows()};
         this.rowGetter = this.rowGetter.bind(this);
     }
     createRows () {
-        this._rows = _.times(5, x => ({
+        return _.times(5, x => ({
             id: x, 
             title: 'Title ' + x, 
             count: x *100}));
     }
     rowGetter (i: number) {
-        return this._rows[i];
+        return this.state._rows[i];
     }
+    handleGridRowsUpdated = ({fromRow, toRow, updated}) => {
+        let rows = this.state._rows.slice();
+        
+        this.setState({ rows });
+    };
     render() {
         return (
             <ReactDataGrid
                 columns={this._columns}
                 rowGetter={this.rowGetter}
-                rowsCount={this._rows.length}
+                rowsCount={this.state._rows.length}
                 minHeight={500}
+                onGridRowsUpdated={this.handleGridRowsUpdated}
                 />
         );
     }
