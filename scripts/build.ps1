9086d6733627aa236e8b6877d720061512ed66f8
Jonathan Tyson <jtyson79473@gmail.com>
2018-08-21 23:59:29 -0400

    changed build to run dotnet publish instead of build

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -7,8 +7,7 @@ $OutDir = "$ServerPath/var/nginx"
 $aquilaDir = "$OutDir/aquila"
 
 Set-Location ./core/
-# TODO: have dotnet build normally, copy files manually
-dotnet build ./Server/Server.fsproj -c Release -o "./bin/core" -f netcoreapp2.0
+dotnet publish ./Server/Server.fsproj -c Release -o "./bin/publish"
 npm run release
 Set-Location ../haskell/yesod/aquila/
 stack build --copy-bins
@@ -16,7 +15,7 @@ $localPath = stack path --local-bin
 Set-Location ../../../
 # copy to destination
 if ($IsProd) {
-	pscp -i C:\Users\jtyso\Downloads\main.ppk -batch -r ./core/Server/bin/core/* "$OutDir/core/"
+	pscp -i C:\Users\jtyso\Downloads\main.ppk -batch -r ./core/Server/bin/publish/* "$OutDir/core/"
 	pscp -i C:\Users\jtyso\Downloads\main.ppk -batch -r ./public/* "$OutDir/public/"
 	pscp -i C:\Users\jtyso\Downloads\main.ppk -batch -r ./haskell/yesod/aquila/static/* $aquilaDir
 	pscp -i C:\Users\jtyso\Downloads\main.ppk -batch -r $localPath/* $aquilaDir
@@ -24,7 +23,7 @@ if ($IsProd) {
 }
 else {
 	Remove-Item -Path "$OutDir/*" -Recurse
-	Copy-Item -Path ./core/Server/bin/core/ -Destination "$OutDir/core/" -Force -Recurse -Container
+	Copy-Item -Path ./core/Server/bin/publish/ -Destination "$OutDir/core/" -Force -Recurse -Container
 	Copy-Item -Path ./public/ -Destination "$OutDir/public/" -Force -Recurse -Container
 	Copy-Item -Path ./haskell/yesod/aquila/static/ -Destination $aquilaDir -Recurse -Force -Container
 	Copy-Item -Path $localPath -Destination $aquilaDir -Force -Recurse -Container
