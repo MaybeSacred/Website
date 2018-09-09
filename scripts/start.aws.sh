72886a6973aa87c2dcb5787eb640f28fb130957e
Jonathan Tyson <jtyson79473@gmail.com>
2018-09-09 14:05:37 -0400

    added support for haskell subsite

--- scripts/start.aws.sh
+++ scripts/start.aws.sh
@@ -1,3 +1,7 @@
 sudo nginx -s reload
 pgrep dotnet | xargs kill -s 15
+cd /var/nginx/core/
+# to overcome current limitation with dotnet core
 screen -d -m -S SERVER bash -c 'dotnet /var/nginx/core/Server.dll -- --port 8080 --homefolder /var/nginx/public'
+cd /var/nginx/aquila/
+aquila --port 3000
