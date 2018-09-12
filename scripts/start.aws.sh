aae425c0aef4e8643064429d7fbc739780057a8d
Jonathan Tyson <jtyson79473@gmail.com>
2018-09-11 23:13:37 -0400

    fixed script so that it would restart programs correctly fixed smaller breakpoints for mobile

--- scripts/start.aws.sh
+++ scripts/start.aws.sh
@@ -1,6 +1,9 @@
+#!/bin/sh
 sudo nginx -s reload
-pgrep -f "(dotnet|aquila)" | xargs kill -s 15
+killall dotnet
+killall aquila
 # to overcome current limitation with dotnet core
-screen -d -m -S SERVER bash -c 'dotnet /var/nginx/core/Server.dll -- --port 8080 --homefolder /var/nginx/public'
-cd /var/nginx/aquila/
-aquila --port 3000
+screen -d -m -S SERVER bash -c 'dotnet /var/nginx/core/Server.dll -- --port 8080 --homefolder /var/nginx/public' 
+
+# cd /var/nginx/aquila/
+# aquila --port 3000
