4893c2ac25255e5d8a848d3f8f716a4b30cef20c
Jonathan Tyson <jtyson79473@gmail.com>
2018-09-20 23:08:57 -0400

    added some notes for qr

--- core/Server/site/Articles.fs
+++ core/Server/site/Articles.fs
@@ -3,39 +3,11 @@ open CommonHtml
 open Suave.FunctionalViewEngine
 open Types
 open System
-
-let ``making-a-qr-code-generator`` = { 
-    Title = "QR Codes for Pokémon" 
-    Created = DateTime.Now
-    Modified = DateTime.Now
-    Content = textify 
-        [
-            div [] [
-                h2 [class' "text-center"] [rawText "Building a QR Generator for Pokémon"]
-                br'
-                a [title' "By en:Game Freak, en:Nintendo, en:The Pokémon Company (Transferred from en.wikipedia to Commons.) [Public domain], via Wikimedia Commons"; href "https://commons.wikimedia.org/wiki/File%3AInternational_Pok%C3%A9mon_logo.svg"; class' "img-responsive center-block"] [
-                    img [attr "width" "256"; attr "alt" "International Pokémon logo"; src "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/256px-International_Pok%C3%A9mon_logo.svg.png"]
-                ]
-                br'
-                br'
-                p [] [
-                    rawText "   The newest generation of Pokémon games, "
-                    a [href "https://www.serebii.net/ultrasunultramoon/"] [rawText "Sun and Moon and their Ultra counterparts"]
-                    rawText """, have a 
-                    feature where Pokémon not normally found within the game can be caught. This is called Island Scan, and each activation of Island Scan requires 
-                    scanning QR codes using the Nintendo DS camera. The game designers were clever though, the 
-                    game won't count scanning the same QR code multiple times, so you'll need many distinct codes if 
-                    you intend to catch all 40ish Pokémon available through the Island Scan feature. This sounded like a fun little problem to solve and ultimately, 
-                    solving it led to the creation of this site"""
-                ]
-                h4 [] [rawText "What's a poor programmer to do?"]
-                paragraph """Make a randomized QR code generator, of course! TL;DR, That's exactly what I did. """
-            ]
-        ] }
+open QRGenerator
 
 let articles = 
     dict [
-        "inaugaral", ``making-a-qr-code-generator``
+        "inaugaral", QRGenerator.``making-a-qr-code-generator``
     ]
 
 let main = {
--- core/Server/site/QRGenerator.fs
+++ core/Server/site/QRGenerator.fs
@@ -50,3 +50,37 @@ let page = {
             ] |> flip List.append [for _ in 0 .. 11 do yield div [class' "col-12 col-md-6 col-lg-4 col-xl-2"] [img [class' "align-middle mx-auto d-block"; src <| base64Qr ()]] ])
         ]
     ] ) }
+
+let ``making-a-qr-code-generator`` = { 
+    Title = "QR Codes for Pokémon" 
+    Created = DateTime(2018,20,9)
+    Modified = DateTime(2018,20,9)
+    Content = textify 
+        [
+            div [] [
+                h2 [class' "text-center"] [rawText "Building a QR Generator for Pokémon"]
+                br'
+                a [title' "By en:Game Freak, en:Nintendo, en:The Pokémon Company (Transferred from en.wikipedia to Commons.) [Public domain], via Wikimedia Commons"; href "https://commons.wikimedia.org/wiki/File%3AInternational_Pok%C3%A9mon_logo.svg"; class' "img-responsive center-block"] [
+                    img [attr "width" "256"; attr "alt" "International Pokémon logo"; src "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/256px-International_Pok%C3%A9mon_logo.svg.png"]
+                ]
+                br'
+                br'
+                p [] [
+                    rawText "   The newest generation of Pokémon games, "
+                    a [href "https://www.serebii.net/ultrasunultramoon/"] [rawText "Sun and Moon and their Ultra counterparts"]
+                    rawText """, have a 
+                    feature where Pokémon not normally found within the game can be caught. This is called Island Scan, and each activation of Island Scan requires 
+                    scanning QR codes using the Nintendo DS camera. The game designers were clever though, the 
+                    game won't count scanning the same QR code multiple times, so you'll need many distinct codes if 
+                    you intend to catch all 40ish Pokémon available through the Island Scan feature. This sounded like a fun little problem to solve and ultimately, 
+                    solving it led to the creation of this site"""
+                ]
+                h4 [] [rawText "What's a poor programmer to do?"]
+                paragraph """Make a randomized QR code generator, of course! TL;DR, That's exactly what I did. """
+                // include hanselman article
+                // https://www.hanselman.com/blog/HowDoYouUseSystemDrawingInNETCore.aspx?utm_source=csharpdigest&utm_medium=email&utm_campaign=featured
+                // describe various attempts to get it working on Linux
+                // include summary of the steps: install packages and install Linux libraries like from hanselman article
+                // include building of api and query params
+            ]
+        ] }
